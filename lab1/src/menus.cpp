e1664cd08602035a01116461206800c73a0ca5dc
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-07 20:38:31 +0300

    Add task menu output

--- lab1/src/menus.cpp
+++ lab1/src/menus.cpp
@@ -6,3 +6,17 @@ void showConstructorsMenu() {
     std::cout << "1.Use default constructor." << std::endl;
     std::cout << "2.Use constructor with parameters." << std::endl;
 }
+
+void showTaskMenu() {
+    std::cout << "\n\t\t\t\tTASK" << std::endl;
+    std::cout << "Create a class for working with strings." << std::endl;
+    std::cout << "Allocate memory for strings dynamically." << std::endl;
+    std::cout << "Define a default constructor and a parameterized constructor." << std::endl;
+    std::cout << "Implement the following methods: input data into a string," << std::endl;
+    std::cout << "display the string, and concatenate strings." << std::endl;
+    std::cout << "\n\t\t\t\tMENU" << std::endl;
+    std::cout << "1.Enter the string." << std::endl;
+    std::cout << "2.Display the string." << std::endl;
+    std::cout << "3.Concatenate the strings." << std::endl;
+    std::cout << "4.Exit the program." << std ::endl;
+}
