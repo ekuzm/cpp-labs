4a423b83e35e50f858005b87ff9d0893e2791d3a
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-07 19:12:00 +0300

    Add new utility functions(resizeString and getString)

--- lab1/include/utils.h
+++ lab1/include/utils.h
@@ -1,3 +1,5 @@
 #pragma once
 
 int myStrlen(const char *str);
+char *resizeString(char *&str, int len);
+char *getString(const char *msg);
--- lab1/src/utils.cpp
+++ lab1/src/utils.cpp
@@ -13,3 +13,53 @@ int myStrlen(const char *str) {
 
     return len;
 }
+
+char *resizeString(char *&str, const int len) {
+    char *new_str = new char[len];
+
+    for (int ind = 0; ind < len; ind++) {
+        new_str[ind] = '\0';
+    }
+
+    for (int ind = 0; str[ind] != '\0'; ind++) {
+        new_str[ind] = str[ind];
+    }
+
+    delete[] str;
+    str = nullptr;
+
+    return new_str;
+}
+
+char *getString(const char *msg) {
+    std ::cout << msg;
+
+    int cap = 1;
+    char *str = new char[cap];
+    int check = 0;
+    int len = 0;
+    str[len] = '\0';
+
+    while (true) {
+        check = std::cin.get();
+        if (check != EOF && (char)check != '\n') {
+            break;
+        }
+        if (len + 1 == cap) {
+            cap *= 2;
+            str = resizeString(str, cap);
+        }
+
+        str[len] = char(check);
+        len++;
+
+        if (str[0] == '\0') {
+            std::cout << "\nError, the string cannot be empty.Please try again: ";
+            len = 0;
+            cap = 1;
+        }
+    }
+
+    str[len] = '\0';
+    return str;
+}
