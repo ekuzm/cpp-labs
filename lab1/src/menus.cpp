d8dcf4b82cf716f616ef9f94631b0511b9dcf433
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-07 19:39:17 +0300

    Add constructors menu output

---
+++ lab1/src/menus.cpp
@@ -0,0 +1,8 @@
+#include "menus.h"
+#include <iostream>
+
+void showConstructorsMenu() {
+    std::cout << "\t\t\tCONSTRUCTORS MENU" << std::endl;
+    std::cout << "1.Use default constructor." << std::endl;
+    std::cout << "2.Use constructor with parameters." << std::endl;
+}
