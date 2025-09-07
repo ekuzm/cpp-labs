25f2eb83126a4d2a3c20b5bb6dee7c70046fc831
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-07 20:09:25 +0300

    Add color constants

---
+++ lab1/include/consts.h
@@ -0,0 +1,5 @@
+#pragma once
+
+inline constexpr const char *kWhiteColor = "\o{33}[0m";
+inline constexpr const char *kRedColor = "\o{33}[31m";
+inline constexpr const char *kGreenColor = "\o{33}[32m";
