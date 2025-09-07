0b7bcf4b17350a0d8f8bd990440b63fd5a40f0ab
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-07 19:13:22 +0300

    Add input, show, concatenate, remove methods to the class String

--- lab1/include/my_string.h
+++ lab1/include/my_string.h
@@ -8,4 +8,9 @@ class String {
   public:
     String();
     String(char *&str);
+
+    void input(const char *msg);
+    void show(const char *msg) const;
+    void concatenate(String str);
+    void remove();
 };
--- lab1/src/my_string.cpp
+++ lab1/src/my_string.cpp
@@ -16,3 +16,45 @@ String::String(char *&str) : len(myStrlen(str)), cap(len + 1) {
     delete[] str;
     str = nullptr;
 }
+
+void String::input(const char *msg) {
+    if (data != nullptr) {
+        delete[] data;
+        data = nullptr;
+    }
+    data = getString(msg);
+    len = myStrlen(data);
+    cap = len + 1;
+}
+
+void String::show(const char *msg) const {
+    if (data == nullptr || len == 0) {
+        std::cout << "\nError, your string is empty." << std::endl;
+    }
+    std::cout << msg;
+    std::cout << data << std::endl;
+}
+
+void String::concatenate(const String str) {
+    if (str.data == nullptr || str.len == 0) {
+        return;
+    }
+    int new_len = len + str.len;
+    cap = new_len + 1;
+
+    data = resizeString(data, cap);
+
+    for (int ind = 0; ind < str.len; ind++) {
+        data[len + ind] = str.data[ind];
+    }
+
+    len = new_len;
+    data[len] = '\0';
+}
+
+void String::remove() {
+    delete[] data;
+    data = nullptr;
+    len = 0;
+    cap = 0;
+}
