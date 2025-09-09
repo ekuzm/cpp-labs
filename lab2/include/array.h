cc22711d1102f8ae9c42d637c714e2e0f161a0ae
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-10 00:44:43 +0300

    Add default constructor, parameterized constructor, destructor, copy constructor, move constructor, assignment operator with copy and assignment operator with move to class Array

--- lab2/include/array.h
+++ lab2/include/array.h
@@ -4,4 +4,13 @@ class Array {
     int *data;
     int size;
     int cap;
+
+  public:
+    Array();
+    Array(int arr_size);
+    ~Array();
+    Array(const Array &other);
+    Array(Array &&move) noexcept;
+    Array &operator=(const Array &other);
+    Array &operator=(Array &&move) noexcept;
 };
--- lab2/src/array.cpp
+++ lab2/src/array.cpp
@@ -1,1 +1,65 @@
 #include "array.h"
+
+Array::Array() : data(nullptr), size(0), cap(0) {
+}
+
+Array::Array(int arr_size) : size(arr_size), cap(size * 2) {
+    data = new int[cap];
+}
+
+Array::~Array() {
+    delete[] data;
+    data = nullptr;
+    size = 0;
+    cap = 0;
+}
+
+Array::Array(const Array &other) : size(other.size), cap(other.cap) {
+    data = new int[cap];
+
+    for (int i = 0; i < size; i++) {
+        data[i] = other.data[i];
+    }
+}
+
+Array::Array(Array &&move) noexcept : data(move.data), size(move.size), cap(move.cap) {
+    move.data = nullptr;
+    move.size = 0;
+    move.cap = 0;
+}
+
+Array &Array::operator=(const Array &other) {
+    if (this != &other) {
+        size = other.size;
+        cap = other.cap;
+
+        delete[] data;
+        data = nullptr;
+
+        if (cap > 0) {
+            data = new int[cap];
+
+            for (int i = 0; i < size; i++) {
+                data[i] = other.data[i];
+            }
+        }
+    }
+
+    return *this;
+}
+
+Array &Array::operator=(Array &&move) noexcept {
+    if (this != &move) {
+        size = move.size;
+        cap = move.cap;
+
+        delete[] data;
+        data = move.data;
+
+        move.data = nullptr;
+        move.size = 0;
+        move.cap = 0;
+    }
+
+    return *this;
+}
