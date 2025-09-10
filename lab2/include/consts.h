a0665f5465f50bf79709da6ab320234aad7fcb2e
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-10 13:38:28 +0300

    Add constants for integer limits and terminal colors

---
+++ lab2/include/consts.h
@@ -0,0 +1,7 @@
+#pragma once
+
+inline constexpr const int kMaxInt = 2147483647;
+inline constexpr const int kMinInt = -2147483648;
+inline constexpr const char *kWhiteColor = "\o{33}[0m";
+inline constexpr const char *kRedColor = "\o{33}[31m";
+inline constexpr const char *kGreenColor = "\o{33}[32m";
