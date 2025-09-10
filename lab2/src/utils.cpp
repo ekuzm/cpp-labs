afaeff2ae5fd3d96a0a34a0c5f6f779c47f51ff0
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-10 13:37:01 +0300

    Add utility function (getNumber)

---
+++ lab2/src/utils.cpp
@@ -0,0 +1,22 @@
+#include "utils.h"
+#include "consts.h"
+#include <iostream>
+
+int getNumber(const char *msg) {
+    int num = 0;
+
+    std::cout << msg;
+
+    while (true) {
+        if (std::cin.peek() == '\n' || std::cin.peek() == ' ' || std::cin.fail()) {
+            std::cin.clear();
+            while (std::cin.get() != '\n' && !std::cin.eof())
+                ;
+            std::cout << kRedColor << "\nError, invalid input. Please try again: " << kWhiteColor;
+            continue;
+        }
+        if ((std::cin >> num).good() && (std::cin.peek() == '\n' || std::cin.peek() == ' ') && (kMinInt <= num) &&
+            (num <= kMaxInt))
+            return num;
+    }
+}
