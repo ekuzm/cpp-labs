7a8ad719ca88c813e496f94e4cf9803cad44a82b
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-13 14:13:26 +0300

    Add passenger carrier voices and colors constants

---
+++ lab3/include/consts.h
@@ -0,0 +1,9 @@
+#pragma once
+
+inline constexpr const char *kCarVoice = "BIP BIP!";
+inline constexpr const char *kTrainVoice = "CHOOH CHOOH!";
+inline constexpr const char *kAirplaneVoice = "WHOOOSH!";
+
+inline constexpr const char *kWhiteColor = "\o{33}[0m]";
+inline constexpr const char *kRedColor = "\o{33}[31m";
+inline constexpr const char *kGreenColor = "\o{33}[32m";
