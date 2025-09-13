5569aabed6aab412dbc8483d163d0c0f8ff4e5d3
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-14 01:07:23 +0300

    Fix bugs

--- lab4/src/cargo_carrier.cpp
+++ lab4/src/cargo_carrier.cpp
@@ -4,14 +4,13 @@ CargoCarrier::CargoCarrier() : costPrKm(0.0), speedKmH(0.0), distance(0.0) {
 }
 
 CargoCarrier::CargoCarrier(double cost, double speed, double travelDistance, std::string cityName)
-    : costPrKm(cost), speedKmH(speed), distance(travelDistance), city((std::move(cityName))) {
+    : costPrKm(cost), speedKmH(speed), distance(travelDistance), city(std::move(cityName)) {
 }
 
 CargoCarrier::~CargoCarrier() {
     costPrKm = 0;
     speedKmH = 0;
     distance = 0;
-    city = "";
 }
 
 CargoCarrier::CargoCarrier(const CargoCarrier &other) = default;
