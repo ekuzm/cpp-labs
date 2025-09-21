52d02514c9dd404a72147e24a343658c244834fc
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-09-21 20:44:29 +0300

    Fix some bugs

--- lab3/src/passenger_carrier.cpp
+++ lab3/src/passenger_carrier.cpp
@@ -1,34 +1,26 @@
 #include "passenger_carrier.h"
 
-PassengerCarrier::PassengerCarrier() : costPrKm(0.0), speedKmH(0.0), distance(0.0) {
-}
+PassengerCarrier::PassengerCarrier()
+    : costPrKm(0.0), speedKmH(0.0), distance(0.0) {}
 
-PassengerCarrier::PassengerCarrier(double cost, double speed, double travelDistance)
-    : costPrKm(cost), speedKmH(speed), distance(travelDistance) {
-}
+PassengerCarrier::PassengerCarrier(double cost, double speed,
+                                   double travelDistance)
+    : costPrKm(cost), speedKmH(speed), distance(travelDistance) {}
 
-PassengerCarrier::~PassengerCarrier() {
-    costPrKm = 0;
-    speedKmH = 0;
-    distance = 0;
-}
+PassengerCarrier::~PassengerCarrier() = default;
 
 PassengerCarrier::PassengerCarrier(const PassengerCarrier &other) = default;
 
 PassengerCarrier::PassengerCarrier(PassengerCarrier &&move) noexcept = default;
 
-PassengerCarrier &PassengerCarrier::operator=(const PassengerCarrier &other) = default;
+PassengerCarrier &PassengerCarrier::operator=(const PassengerCarrier &other) =
+    default;
 
-PassengerCarrier &PassengerCarrier::operator=(PassengerCarrier &&move) noexcept = default;
+PassengerCarrier &PassengerCarrier::operator=(
+    PassengerCarrier &&move) noexcept = default;
 
-std::string PassengerCarrier::getSound() const {
-    return "NONE";
-}
+std::string PassengerCarrier::getSound() const { return "NONE"; }
 
-double PassengerCarrier::calculateCost() const {
-    return distance * costPrKm;
-}
+double PassengerCarrier::calculateCost() const { return distance * costPrKm; }
 
-double PassengerCarrier::calculateTime() const {
-    return distance / speedKmH;
-}
+double PassengerCarrier::calculateTime() const { return distance / speedKmH; }
